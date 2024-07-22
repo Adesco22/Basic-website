51b2fd7b3c1e26b941b1d4679fdd20cd539c0991
unknown <you@example.com>
2024-07-22 14:04:39 +0100

    js was updated

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-var bankAcc = parseInt(prompt("Enter your acc"))
-function makeTrans(priceofsale) {
+// var bankAcc = parseInt(prompt("Enter your acc"))
+// function makeTrans(priceofsale) {
 
-    if (priceofsale <= bankAcc) {
-        bankAcc -= priceofsale;
-        alert('$' + bankAcc + ' made the Transcation Successful!!!');
-        console.log('Transcation Successful!!!');
-    } else {
-        alert('$' + bankAcc + ' has Insufficient funds!');
-        console.log('Insufficient funds!');
-    }
-}
+//     if (priceofsale <= bankAcc) {
+//         bankAcc -= priceofsale;
+//         alert('$' + bankAcc + ' made the Transcation Successful!!!');
+//         console.log('Transcation Successful!!!');
+//     } else {
+//         alert('$' + bankAcc + ' has Insufficient funds!');
+//         console.log('Insufficient funds!');
+//     }
+// }
 
-let result = makeTrans();
+// let result = makeTrans();
 
-console.log(result)
+// console.log(result)
